871d4fb1245d711358fd5aa7fda4653a9742c6cc
Salim Khan <salim.khan@mobulous.com>
2024-10-11 15:36:12 +0530

    types of function

---
+++ Function/function.js
@@ -0,0 +1,31 @@
+
+// single line function
+
+// const number=(a,b)=> a+b
+
+// anonimous function
+
+// const number=function(a,b){
+//     return a+b
+// }
+
+// ===========named function===========
+function number(a,b){
+    return a+b;
+}
+
+// ==================highorder function=========
+
+// function number(a,b){
+//     return a+b;
+// }
+
+// function highorder(number){
+//     return number(2,6)
+// }
+
+
+// console.log(highorder(number))
+
+
+
